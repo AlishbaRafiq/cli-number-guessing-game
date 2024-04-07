18d533af7dda2faf3f9009fdb159cd5b60e6f4b7
AlishbaRafiq <165753406+AlishbaRafiq@users.noreply.github.com>
2024-04-07 16:23:00 +0500

    create cli-based-number-guessing-game

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1. computer will generate a random number - Done
+// 2. computer will ask the user to guess the random number - Done
+// compare user input with computer generated random number and show the result -Done
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10: ",
+    },
+]);
+// conditional statements 
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congrats! You got it!");
+}
+else {
+    console.log("Sorry, try again!");
+}
